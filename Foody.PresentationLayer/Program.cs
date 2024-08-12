fc1b63de6fba0b2385f4c47eca6b474b5e2190d5
Ahmet Sakallıoğlu <ahmetsakallioglu54@gmail.com>
2024-08-12 14:33:35 +0300

    Product List

--- Foody.PresentationLayer/Program.cs
+++ Foody.PresentationLayer/Program.cs
@@ -10,8 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<FoodyContext>();
 
-builder.Services.AddScoped<ICategoryDal,EfCategoryDal>();
-builder.Services.AddScoped<ICategoryService,CategoryManager>();
+builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+
+builder.Services.AddScoped<IProductDal, EfProductDal>();
+builder.Services.AddScoped<IProductService, ProductManager>();
 
 builder.Services.AddControllersWithViews();
 
